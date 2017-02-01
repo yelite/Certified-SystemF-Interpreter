244872b293f03a2d020e6d2bd6dc2cb5da3f9988
Lite Ye <lye@cs.umass.edu>
2017-01-31 19:36:13 -0500

    Simplify quote_type proof

--- unquote.v
+++ unquote.v
@@ -5,7 +5,8 @@ Require Import lib.
 Definition injection_symbol : ascii := "i".
 Definition injection_var : exp := (exp_var "i").
 
-Fixpoint _quote (t_env: type_env) (e: exp) : option exp :=
+
+Fixpoint _quote (t_env : type_env) (e : exp) : option exp :=
   match e with
   | exp_func v t_v e =>
       let new_t_env := { v, t_v | t_env } in
@@ -46,6 +47,7 @@ Definition unquote : exp :=
   let rtype := type_func identity_type (type_var "a") in
   exp_tfunc "a" (exp_func "q" rtype
                           (exp_app (exp_var "q") identity)).
+
 
 Lemma strong_quote_nf : forall e e' t_env,
     _quote t_env e = Some e' ->
@@ -107,53 +109,31 @@ Proof.
   generalize dependent t0.
   induction e; intros t0 env Ht e0' He; simpl in He;
     try unfold option_map in He;
-    inversion Ht; subst; inversion Hi.
-  - remember {a, t | env} as new_env.
-    remember (_quote new_env e) as oe'.
-    destruct oe' as [e'|]; inversion He.
-    constructor.
+    inversion Ht; subst; inversion Hi; destruct_match; auto.
+  - constructor.
     rewrite (mapping_reorder _ _ _ _ _ H1).
-    rewrite <- Heqnew_env.
-    auto.
-  - remember (_typecheck env e1) as ot1.
-    remember (_quote env e1) as oe1'.
-    remember (_quote env e2) as oe2'.
-    destruct ot1 as [t1|]; inversion He.
-    destruct oe1' as [e1'|]; inversion He.
-    destruct oe2' as [e2'|]; inversion He.
     subst.
-    apply tc_app with t_v;
-      try apply tc_app with (type_func t_v t0);
-      try apply tc_tapp with
-      identity_type_symbol identity_inner_type;
-      auto.
+    auto.
+  - apply tc_app with t_v;
+      try apply tc_app with (type_func t_v t0); eauto.
     (* i t1 |- t1 -> t1 *)
     apply typecheck_complete in H2.
-    rewrite H2 in Heqot1.
-    inversion Heqot1.
-    auto.
-  - remember (_quote env e) as oe'.
-    destruct oe' as [e'|]; inversion He.
-    auto.
-  - remember (_typecheck env e) as ot'.
-    remember (_quote env e) as oe'.
-    destruct ot' as [t'|]; inversion He.
-    destruct oe' as [e'|]; inversion He.
-    apply tc_tapp with v t_e; auto.
+    rewrite H2 in *.
+    solve_by_inversion_step auto.
+  - apply tc_tapp with v t_e; auto.
     apply tc_app with (type_forall v t_e); auto.
-    apply tc_tapp with
-    identity_type_symbol identity_inner_type; auto.
+    apply tc_tapp with identity_type_symbol identity_inner_type; auto.
     apply typecheck_complete in H2.
-    rewrite H2 in Heqot'.
-    inversion Heqot'.
-    auto.
+    rewrite H2 in *.
+    solve_by_inversion_step auto.
   - inversion He.
     constructor.
     unfold mapping_update.
-    destruct (ascii_dec a injection_symbol).
-    + contradiction.
-    + auto.
+    destruct_prem.
+    auto.
 Qed.
+
+Hint Resolve strong_quote_type.
 
 
 Theorem quote_type : forall e e' t0,
@@ -162,20 +142,12 @@ Theorem quote_type : forall e e' t0,
     typecheck e = Some t0 ->
     typecheck e' = Some (type_func identity_type t0).
 Proof.
-  intros e e' t0 H0 H1.
+  intros e e' t0 H0 H1 H2.
   unfold quote in H1.
   unfold option_map in H1.
-  remember (_quote empty_mapping e) as oe'.
-  destruct oe'; destruct e'; inversion H1.
-  subst. clear H1.
-  intros H2.
+  destruct_match.
   apply typecheck_complete.
-  constructor.
-  apply typecheck_sound.
-  eapply strong_quote_type.
-  apply H0.
-  auto.
-  auto.
+  eauto.
 Qed.
 
 
